14b3038dc5c3cdd02f84bc45fd6999a719a63852
Heini Emilia <hei@heiniemilia.fi>
2025-10-06 17:02:16 +0300

    -

--- car-modals.js
+++ car-modals.js
@@ -25,34 +25,34 @@ const manualCarsMap = {
     {id:"man4",title:"Toyota Yaris",image:"img/cars/yaris-heri-susilo-kwgTG2TETPs-unsplash.webp",info:{"Valmistettu":"2020","Korimalli":"Hatchback",Ovia:5,Istuimia:5},extraImages:[]}
   ],
   "pakettiautot.html": [
-    {id:"man1",title:"Volkswagen Touran",image:"img/cars/vw-touran.jpg",info:{"Valmistettu":"2019","Korimalli":"MPV",Ovia:5,Istuimia:7},extraImages:[]},
-    {id:"man2",title:"Ford Transit Connect",image:"img/cars/ford-transit-connect.jpg",info:{"Valmistettu":"2020","Korimalli":"Van",Ovia:4,Istuimia:2},extraImages:[]},
-    {id:"man3",title:"Mercedes-Benz V-Class",image:"img/cars/mercedes-v-class.jpg",info:{"Valmistettu":"2021","Korimalli":"Minivan",Ovia:5,Istuimia:8},extraImages:[]},
-    {id:"man4",title:"Toyota ProAce",image:"img/cars/toyota-proace.jpg",info:{"Valmistettu":"2022","Korimalli":"Van",Ovia:4,Istuimia:3},extraImages:[]}
+    {id:"man1",title:"Volkswagen Touran",image:"https://upload.wikimedia.org/wikipedia/commons/4/40/VW_Touran_Highline_Black_Style.jpg",info:{"Valmistettu":"2019","Korimalli":"MPV",Ovia:5,Istuimia:7},extraImages:[]},
+    {id:"man2",title:"Ford Transit Connect",image:"https://upload.wikimedia.org/wikipedia/commons/2/2e/Ford_Transit_Connect_--_08-25-2009.jpg",info:{"Valmistettu":"2020","Korimalli":"Van",Ovia:4,Istuimia:2},extraImages:[]},
+    {id:"man3",title:"Mercedes-Benz V-Class",image:"https://upload.wikimedia.org/wikipedia/commons/a/af/2011_Mercedes-Benz_Viano_%28V_639_MY12%29_Edition_125_CDI_3.0_BlueEFFICIENCY_van_%282015-07-24%29_01.jpg",info:{"Valmistettu":"2021","Korimalli":"Minivan",Ovia:5,Istuimia:8},extraImages:[]},
+    {id:"man4",title:"Toyota ProAce",image:"https://upload.wikimedia.org/wikipedia/commons/e/e5/SLM_65_-_Toyota_ProAce.jpg",info:{"Valmistettu":"2022","Korimalli":"Van",Ovia:4,Istuimia:3},extraImages:[]}
   ],
   "urheiluautot.html": [
-    {id:"man1",title:"Porsche 911",image:"img/cars/porsche911.jpg",info:{"Valmistettu":"2022","Korimalli":"Coupe",Ovia:2,Istuimia:2},extraImages:[]},
-    {id:"man2",title:"Audi R8",image:"img/cars/audi-r8.jpg",info:{"Valmistettu":"2021","Korimalli":"Coupe",Ovia:2,Istuimia:2},extraImages:[]},
-    {id:"man3",title:"Ferrari Portofino",image:"img/cars/ferrari-portofino.jpg",info:{"Valmistettu":"2020","Korimalli":"Cabriolet",Ovia:2,Istuimia:2},extraImages:[]},
-    {id:"man4",title:"Mercedes-AMG GT",image:"img/cars/mercedes-amg-gt.jpg",info:{"Valmistettu":"2022","Korimalli":"Coupe",Ovia:2,Istuimia:2},extraImages:[]}
+    {id:"man1",title:"Porsche 911",image:"https://upload.wikimedia.org/wikipedia/commons/1/19/2015_Porsche_911_Carrera_4S_Coupe.jpg",info:{"Valmistettu":"2022","Korimalli":"Coupe",Ovia:2,Istuimia:2},extraImages:[]},
+    {id:"man2",title:"Audi R8",image:"https://upload.wikimedia.org/wikipedia/commons/3/36/2015_Audi_R8_Coup%C3%A9_5.2_FSI_quattro_%2819409896583%29.jpg",info:{"Valmistettu":"2021","Korimalli":"Coupe",Ovia:2,Istuimia:2},extraImages:[]},
+    {id:"man3",title:"Ferrari Portofino",image:"https://upload.wikimedia.org/wikipedia/commons/9/99/Ferrari_Portofino_IMG_0531.jpg",info:{"Valmistettu":"2020","Korimalli":"Cabriolet",Ovia:2,Istuimia:2},extraImages:[]},
+    {id:"man4",title:"Mercedes-AMG GT",image:"https://upload.wikimedia.org/wikipedia/commons/4/49/2015-2017_Mercedes-AMG_GT_%28C_190%29_S_coupe_%282017-07-15%29_01.jpg",info:{"Valmistettu":"2022","Korimalli":"Coupe",Ovia:2,Istuimia:2},extraImages:[]}
   ],
   "maastoautot.html": [
-    {id:"man1",title:"Jeep Wrangler",image:"img/cars/jeep_wrangler.jpg",info:{"Valmistettu":"2017","Korimalli":"SUV",Ovia:4,Istuimia:5},extraImages:[]},
-    {id:"man2",title:"Toyota RAV4",image:"img/cars/rav4.jpg",info:{"Valmistettu":"2021","Korimalli":"Crossover",Ovia:5,Istuimia:5},extraImages:[]},
-    {id:"man3",title:"Land Rover Discovery",image:"img/cars/discovery.jpg",info:{"Valmistettu":"2019","Korimalli":"SUV",Ovia:5,Istuimia:7},extraImages:[]},
-    {id:"man4",title:"Nissan X-Trail",image:"img/cars/xtrail.jpg",info:{"Valmistettu":"2020","Korimalli":"SAV",Ovia:5,Istuimia:5},extraImages:[]}
+    {id:"man1",title:"Jeep Wrangler",image:"https://upload.wikimedia.org/wikipedia/commons/f/f4/2018_Jeep_Wrangler_Unlimited_Sport_4-door_front_6.10.18.jpg",info:{"Valmistettu":"2017","Korimalli":"SUV",Ovia:4,Istuimia:5},extraImages:[]},
+    {id:"man2",title:"Toyota RAV4",image:"https://upload.wikimedia.org/wikipedia/commons/6/6d/2019_Toyota_RAV4_XLE_AWD%2C_front_12.31.19.jpg",info:{"Valmistettu":"2021","Korimalli":"Crossover",Ovia:5,Istuimia:5},extraImages:[]},
+    {id:"man3",title:"Land Rover Discovery",image:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Land_Rover_Discovery_4_HSE_2016.jpg",info:{"Valmistettu":"2019","Korimalli":"SUV",Ovia:5,Istuimia:7},extraImages:[]},
+    {id:"man4",title:"Nissan X-Trail",image:"https://upload.wikimedia.org/wikipedia/commons/3/35/Nissan_X-Trail_%28T33%29_1X7A7179.jpg",info:{"Valmistettu":"2020","Korimalli":"SAV",Ovia:5,Istuimia:5},extraImages:[]}
   ],
   "lava-autot.html": [
-    {id:"man1",title:"Ford Ranger",image:"img/cars/ford-ranger.jpg",info:{"Valmistettu":"2021","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]},
-    {id:"man2",title:"Toyota Hilux",image:"img/cars/toyota-hilux.jpg",info:{"Valmistettu":"2020","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]},
-    {id:"man3",title:"Nissan Navara",image:"img/cars/nissan-navara.jpg",info:{"Valmistettu":"2019","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]},
-    {id:"man4",title:"Mitsubishi L200",image:"img/cars/mitsubishi-l200.jpg",info:{"Valmistettu":"2021","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]}
+    {id:"man1",title:"Ford Ranger",image:"https://upload.wikimedia.org/wikipedia/commons/0/02/2019_Ford_Ranger_XLT_Super_Cab_FX4_front_6.1.19.jpg",info:{"Valmistettu":"2021","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]},
+    {id:"man2",title:"Toyota Hilux",image:"https://upload.wikimedia.org/wikipedia/commons/b/ba/2016_Toyota_HiLux_Invincible_D-4D_4WD_2.4_Rear.jpg",info:{"Valmistettu":"2020","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]},
+    {id:"man3",title:"Nissan Navara",image:"https://upload.wikimedia.org/wikipedia/commons/2/22/Nissan_Navara_Pickup.jpg",info:{"Valmistettu":"2010","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]},
+    {id:"man4",title:"Mitsubishi L200",image:"https://upload.wikimedia.org/wikipedia/commons/7/78/Mitsubishi_L200_Dakar_HP_2021_%2852325676526%29.jpg",info:{"Valmistettu":"2021","Korimalli":"Pick-up",Ovia:4,Istuimia:5},extraImages:[]}
   ],
   "miniautot.html": [
-    {id:"man1",title:"Renault Twizy",image:"img/cars/renault-twizy.jpg",info:{"Valmistettu":"2021","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]},
-    {id:"man2",title:"Ligier JS50",image:"img/cars/ligier-js50.jpg",info:{"Valmistettu":"2020","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]},
-    {id:"man3",title:"Microcar M.Go",image:"img/cars/microcar-mgo.jpg",info:{"Valmistettu":"2022","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]},
-    {id:"man4",title:"Aixam City",image:"img/cars/aixam-city.jpg",info:{"Valmistettu":"2021","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]}
+    {id:"man1",title:"Renault Twizy",image:"https://upload.wikimedia.org/wikipedia/commons/c/c8/Renault_Twizy_electric_car.jpg",info:{"Valmistettu":"2021","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]},
+    {id:"man2",title:"Ligier JS50",image:"https://upload.wikimedia.org/wikipedia/commons/7/70/Ligier_JS50_%28MSP15%29.JPG",info:{"Valmistettu":"2020","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]},
+    {id:"man3",title:"Microcar M.Go",image:"https://upload.wikimedia.org/wikipedia/commons/4/42/Microcar_M.GO_Dynamic_%28MSP15%29.JPG",info:{"Valmistettu":"2022","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]},
+    {id:"man4",title:"Aixam Mega",image:"https://upload.wikimedia.org/wikipedia/commons/e/e4/Aixam_Mega.JPG",info:{"Valmistettu":"2021","Korimalli":"Quadricycle",Ovia:2,Istuimia:2},extraImages:[]}
   ]
 
 
@@ -100,7 +100,7 @@ async function fetchCarsFromAPI(){
   const options={
     method:"GET",
     headers:{
-      "x-rapidapi-key":"b43b477174msh880f765341251b6p1fa2b0jsnf445753a6ca4",
+      "x-rapidapi-key":"df8ba94c97mshb76f9c896aa0450p1ce858jsnee6b335c4888",
       "x-rapidapi-host":"cars-database-with-image.p.rapidapi.com"
     }
   };
@@ -169,7 +169,7 @@ async function openCarModal(car){
       const options={
         method:"GET",
         headers:{
-          "x-rapidapi-key":"b43b477174msh880f765341251b6p1fa2b0jsnf445753a6ca4",
+          "x-rapidapi-key":"df8ba94c97mshb76f9c896aa0450p1ce858jsnee6b335c4888",
           "x-rapidapi-host":"cars-database-with-image.p.rapidapi.com"
         }
       };
